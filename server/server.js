02668304717f2f7b64469b26205c0e5631c24753
josephquested <josephquested@gmail.com>
2016-02-26 14:57:55 +1300

    Add working form and handlebars

--- server/server.js
+++ server/server.js
@@ -1,48 +1,53 @@
 var express = require('express')
 var fs = require('fs')
 var path = require('path')
-var handlebars = require('handlebars')  // is that ok joseph?
+var exphbs = require('express-handlebars')
+var bodyParser = require('body-parser')
 
 var server = express()
+var dataPath = path.join(__dirname, '../data/db.json')
+var viewPath = path.join(__dirname, '/views')
 
+// setup middleware
+
+server.engine('handlebars', exphbs({defaultLayout: 'main'}))
+server.set('view engine', 'handlebars')
+server.set('views', viewPath)
+server.use(bodyParser.urlencoded({ extended: false}))
+server.use(bodyParser.json())
 server.use(express.static(path.join(__dirname, '../client')))
-// server.set('views', (path.join(__dirname, '../views')))
-// server.set('view engine', 'handlebars')
-
-var bogusData = { "fruit": "apple" }
-
 
 // routes
 
 server.get('/', function (req, res) {
-  fs.readFile(__dirname + '/../data/db.json',  'utf8', function (err, data) {
-      console.log(data)
-      res.send(JSON.parse(data))
+  fs.readFile(dataPath, 'utf8', function (err, data) {
+      var books = JSON.parse(data)
+      console.log('books', books)
+      console.log('viewpath', viewPath)
+      res.render('home', {books: books})
   })
         // res.send("hello world")
 })
 
 server.get('/books', function (req, res) {
-  fs.readFile(__dirname + '/../data/db.json',  'utf8', function (err, data) {
+  fs.readFile(dataPath, 'utf8', function (err, data) {
       console.log("in /books")
       res.send("in /books")
   })
 })
 
 server.get('/books/read', function (req, res) {
-  fs.readFile(__dirname + '/../data/db.json',  'utf8', function (err, data) {
+  fs.readFile(dataPath,  'utf8', function (err, data) {
       console.log("in /books/read")
       res.send("in /books/read")
   })
 })
 
 server.get('/books/unread', function (req, res) {
-  fs.readFile(__dirname + '/../data/db.json',  'utf8', function (err, data) {
+  fs.readFile(dataPath,  'utf8', function (err, data) {
       console.log("/books/unread")
       res.send("/books/unread")
   })
 })
-
-
 
 module.exports = server
